7043ff2c345cdfcbbfc771d8b32a564e8c6032e7
IKoporkh <ikoporkh@beeline.kg>
2022-11-27 13:52:19 +0600

    Initial commit

---
+++ src/test/java/com/igorkiv/reactivebeerclient/ReactiveBeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorkiv.reactivebeerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveBeerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
